c59c2f95709a74702810655a87e9673692dbe08d
Shahaan Khan <shahaanzia1@gmail.com>
2026-09-01 11:05:03 -0400

    Updated to create nav system, testing it currently

--- Labs/streamlit_app.py
+++ Labs/streamlit_app.py
@@ -8,46 +8,36 @@ st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+st.set_page_config(
+    page_title="Base", 
+    page_icon="📄", 
+    layout="centered", 
+    initial_sidebar_state="expanded", 
+    menu_items=None)
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+lab_1_page = st.Page(
+    'Labs\\Lab 1\\szkhan-Lab-1.py',
+    title= "Bob",
+    icon= "📄",
+    default=False
     )
-
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
+lab_2_page = st.Page(
+    'Labs\\Lab 2\\szkhan-Lab-2.py',
+    title= "Bob",
+    icon= "📄",
+    default=False
     )
+pgs = st.navigation(
+    [
+        lab_1_page,
+        lab_2_page,
+    ],
+    default=lab_1_page,
+    title="Labs",
+    icon="📄",
+)
 
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-5-nano",
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+st.set_page_config(
+    page_title="Base",
+    page_icon="📄")
+pg.run()
